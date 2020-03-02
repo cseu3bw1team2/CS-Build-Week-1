b87a4a1ba12b3d51f046013d19ca364864cddcea
Petar Scepanovic <hiborija@gmail.com>
2020-03-02 15:18:30 +0100

    added adventure folder and hooked up with settings

---
+++ adv_project/adventure/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+
+"""
+commented out for now because api has not yet been implemented, but this is how it should look when api
+gets implemented and what the methods will be
+
+"""
+
+# from . import api
+
+# urlpatterns = [
+#     url('init', api.initialize),
+#     url('move', api.move),
+#     url('say', api.say),
+
+#     url('fetch_rooms', api.fetchRoom),
+# ]
