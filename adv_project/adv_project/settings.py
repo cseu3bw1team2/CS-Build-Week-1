774969805cb524260ade850185385c4794420a0d
Petar Scepanovic <hiborija@gmail.com>
2020-03-03 18:42:36 +0100

    update

--- adv_project/adv_project/settings.py
+++ adv_project/adv_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,3 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
