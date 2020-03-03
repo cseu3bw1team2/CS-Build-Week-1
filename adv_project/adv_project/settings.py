6ef45bf3f13717aa15ecdfc145250b92fb797f4b
Petar Scepanovic <hiborija@gmail.com>
2020-03-03 19:11:20 +0100

    new2

--- adv_project/adv_project/settings.py
+++ adv_project/adv_project/settings.py
@@ -97,17 +97,17 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adv',
-        'USER': 'postgres',
-        'PASSWORD': 'kudrinica18',
-        'HOST': 'localhost'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'adv',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'kudrinica18',
+    #     'HOST': 'localhost'
+    # }
 
 }
 
